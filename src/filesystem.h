e775ce8230afbb0c63e3fb671d1fac6c82618b32
Ivan <ipkusliy@gmail.com>
2025-12-12 11:26:24 +0300

    added ATTiny1614 code

--- src/filesystem.h
+++ src/filesystem.h
@@ -17,8 +17,8 @@ bool initFilesystem();
 
 // y = x + p/100 * (x^3 - x)
 struct ExponentAndMax{
-  int8_t exponent; /** Value from -100 to 100. if negative, we just reflect the function over y=x (point a;b becomes b;a). */
-  uint8_t maxValue {100}; /** Value 0 - 100, 100 by default */
+  int exponent; /** Value from -100 to 100. if negative, we just reflect the function over y=x (point a;b becomes b;a). */
+  int maxValue {100}; /** Value 0 - 100, 100 by default */
 };
 
 struct Model{
@@ -29,12 +29,12 @@ struct Model{
   bool reversedSwitches2[4]; /** true if reversed */
   bool reversedSwitches3[4]; /** true if reversed */
 
-  int8_t flightModeSwitch2; /** -1 if no fly mode switch is assigned. Only 2-state switches can be assigned. */
-  int8_t throttleHoldSwitch2; /** -1 if no throttle hold switch is assigned. Only 2-state switches can be assigned. */
+  int flightModeSwitch2; /** -1 if no fly mode switch is assigned. Only 2-state switches can be assigned. */
+  int throttleHoldSwitch2; /** -1 if no throttle hold switch is assigned. Only 2-state switches can be assigned. */
   ExponentAndMax flightMode1[6];
   ExponentAndMax flightMode2[6];
 
-  int8_t subtrim[6]; /** values -100 to +100, correcting the input of each variable channel */
+  int subtrim[6]; /** values -100 to +100, correcting the input of each variable channel */
 };
 
 /**
--- src/navigation/navigation.cpp
+++ src/navigation/navigation.cpp
@@ -33,38 +33,43 @@ void saveModelChanges(std::function<void()> callback){
 
 // ================ Main ===========================
 static GEMPage PAGEMain("Main Menu");
-static GEMItem LINKSystemSettings("System Settings", systemSettings);
-static GEMItem LINKFunctionsSetup("Functions Setup", systemSettings);
 
 // ================ System Settings ================
 static GEMPage PAGESystemSettings("System Settings", PAGEMain);
+static GEMItem LINKSystemSettings("System Settings", PAGESystemSettings);
 
-static GEMItem LINKSystemModelNew("New model", systemSettings);
+static GEMPage PAGESystemModelNew("New model", PAGESystemSettings);
+static GEMItem LINKSystemModelNew("New model", PAGESystemModelNew);
 static char INPUTSystemModelNewNameVal[GEM_STR_LEN] = "";
 static GEMItem INPUTSystemModelNewName("Name:", INPUTSystemModelNewNameVal);
 static char INPUTSystemModelNewKeyVal[GEM_STR_LEN] = "";
 static GEMItem INPUTSystemModelNewKey("Secret key:", INPUTSystemModelNewKeyVal);
 static GEMItem BUTTONSystemModelNew("Create", saveSystemChanges);
 
-// static GEMItem LINKSystemModelSelect("Select model", systemSettings); TODO
+static GEMPage PAGESystemModelSelect("Select model", PAGESystemSettings);
+static GEMItem LINKSystemModelSelect("Select model", PAGESystemModelSelect);
 
-static GEMItem LINKSystemModelUpdate("Update model", systemSettings);
+static GEMPage PAGESystemModelUpdate("Update model", PAGESystemSettings);
+static GEMItem LINKSystemModelUpdate("Update model", PAGESystemModelUpdate);
 static char INPUTSystemModelUpdateNameVal[GEM_STR_LEN] = "";
 static GEMItem INPUTSystemModelUpdateName("Name:", INPUTSystemModelUpdateNameVal);
 static char INPUTSystemModelUpdateKeyVal[GEM_STR_LEN] = "";
 static GEMItem INPUTSystemModelUpdateKey("Secret key:", INPUTSystemModelUpdateKeyVal);
 static GEMItem BUTTONSystemModelUpdate("Update", saveSystemChanges);
 
-static GEMItem LINKSystemModelCopy("Copy model", systemSettings);
+static GEMPage PAGESystemModelCopy("Copy model", PAGESystemSettings);
+static GEMItem LINKSystemModelCopy("Copy model", PAGESystemModelCopy);
 static char INPUTSystemModelCopyNameVal[GEM_STR_LEN] = "";
 static GEMItem INPUTSystemModelCopyName("Copy name:", INPUTSystemModelCopyNameVal);
 static GEMItem BUTTONSystemModelCopy("Create a copy", saveSystemChanges);
 
-static GEMItem LINKSystemModelRemove("Remove model", systemSettings);
-static GEMItem TEXTSystemModelRemove("Are you sure you want to remove this model?");
+static GEMPage PAGESystemModelRemove("Remove model", PAGESystemSettings);
+static GEMItem LINKSystemModelRemove("Remove model", PAGESystemModelRemove);
+static GEMItem TEXTSystemModelRemove("Are you sure?");
 static GEMItem BUTTONSystemModelRemove("Yes, Remove", saveSystemChanges);
 
-static GEMItem LINKSystemAuxSwitches("Aux Switches On/Off", systemSettings);
+static GEMPage PAGESystemAuxSwitches("Aux Switches On/Off", PAGESystemSettings);
+static GEMItem LINKSystemAuxSwitches("Aux Switches On/Off", PAGESystemAuxSwitches);
 static GEMItem INPUTSystemAuxSwitchesA("Switch A:", systemSettings.switches2On[static_cast<uint8_t>(Switch2::A)], saveSystemChanges);
 static GEMItem INPUTSystemAuxSwitchesB("Switch B:", systemSettings.switches2On[static_cast<uint8_t>(Switch2::B)], saveSystemChanges);
 static GEMItem INPUTSystemAuxSwitchesC("Switch C:", systemSettings.switches2On[static_cast<uint8_t>(Switch2::C)], saveSystemChanges);
@@ -76,14 +81,17 @@ static GEMItem INPUTSystemAuxSwitchesH("Switch H:", systemSettings.switches3On[s
 
 static GEMItem INPUTSystemSoundEffects("Sound:", systemSettings.soundEffects, saveSystemChanges);
 
-static GEMItem LINKSystemFactoryReset("Factory reset", systemSettings);
-static GEMItem TEXTSystemFactoryReset("Are you sure you want to perform a factory reset?");
+static GEMPage PAGESystemFactoryReset("Factory reset", PAGESystemSettings);
+static GEMItem LINKSystemFactoryReset("Factory reset", PAGESystemFactoryReset);
+static GEMItem TEXTSystemFactoryReset("Are you sure?");
 static GEMItem BUTTONSystemFactoryReset("Yes, Erase All Data", saveSystemChanges);
 
 // ================ Functions Setup ================ TODO Display & Dual expo
 static GEMPage PAGEFunctionsSetup("Functions Setup", PAGEMain);
+static GEMItem LINKFunctionsSetup("Functions Setup", PAGEFunctionsSetup);
 
-static GEMItem LINKFunctionsReverse("Reverse channels", systemSettings);
+static GEMPage PAGEFunctionsReverse("Reverse channels", PAGEFunctionsSetup);
+static GEMItem LINKFunctionsReverse("Reverse channels", PAGEFunctionsReverse);
 static GEMItem INPUTFunctionsReverse1("Vr Channel 1:", selectedModel.reversedPotentiometers[static_cast<uint8_t>(Potentiometer::secondVertical)], saveModelChanges);
 static GEMItem INPUTFunctionsReverse2("Vr Channel 2:", selectedModel.reversedPotentiometers[static_cast<uint8_t>(Potentiometer::secondHorizontal)], saveModelChanges);
 static GEMItem INPUTFunctionsReverse3("Vr Channel 3:", selectedModel.reversedPotentiometers[static_cast<uint8_t>(Potentiometer::firstVertical)], saveModelChanges);
@@ -99,28 +107,109 @@ static GEMItem INPUTFunctionsReverseF("Switch F:", selectedModel.reversedSwitche
 static GEMItem INPUTFunctionsReverseG("Switch G:", selectedModel.reversedSwitches3[static_cast<uint8_t>(Switch3::G)], saveModelChanges);
 static GEMItem INPUTFunctionsReverseH("Switch H:", selectedModel.reversedSwitches3[static_cast<uint8_t>(Switch3::H)], saveModelChanges);
 
-static GEMItem LINKFunctionsSubtrim("Subtrim", systemSettings);
-static GEMItem INPUTFunctionsReverse1("Vr Channel 1:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::secondVertical)], saveModelChanges);
-static GEMItem INPUTFunctionsReverse2("Vr Channel 2:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::secondHorizontal)], saveModelChanges);
-static GEMItem INPUTFunctionsReverse3("Vr Channel 3:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::firstVertical)], saveModelChanges);
-static GEMItem INPUTFunctionsReverse4("Vr Channel 4:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::firstHorizontal)], saveModelChanges);
-static GEMItem INPUTFunctionsReverse5("Vr Channel 5:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::variable1)], saveModelChanges);
-static GEMItem INPUTFunctionsReverse6("Vr Channel 6:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::variable2)], saveModelChanges);
+static GEMPage PAGEFunctionsSubtrim("Subtrim", PAGEFunctionsSetup);
+static GEMItem LINKFunctionsSubtrim("Subtrim", PAGEFunctionsSubtrim);
+static GEMItem INPUTFunctionsSubtrim1("Vr Channel 1:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::secondVertical)], saveModelChanges);
+static GEMItem INPUTFunctionsSubtrim2("Vr Channel 2:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::secondHorizontal)], saveModelChanges);
+static GEMItem INPUTFunctionsSubtrim3("Vr Channel 3:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::firstVertical)], saveModelChanges);
+static GEMItem INPUTFunctionsSubtrim4("Vr Channel 4:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::firstHorizontal)], saveModelChanges);
+static GEMItem INPUTFunctionsSubtrim5("Vr Channel 5:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::variable1)], saveModelChanges);
+static GEMItem INPUTFunctionsSubtrim6("Vr Channel 6:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::variable2)], saveModelChanges);
 
-static GEMItem LINKFunctionsEndPoints("End points", systemSettings);
+// static GEMItem LINKFunctionsEndPoints("End points", systemSettings); TODO
 
-static GEMItem LINKFunctionsAssignSwitches("Assign Switches", systemSettings);
-static GEMItem INPUTFunctionsReverse5("Flight mode:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::variable1)], saveModelChanges);
-static GEMItem INPUTFunctionsReverse6("Throttle hold:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::variable2)], saveModelChanges);
+static GEMPage PAGEFunctionsAssignSwitches("Assign Switches", PAGEFunctionsSetup);
+static GEMItem LINKFunctionsAssignSwitches("Assign Switches", PAGEFunctionsAssignSwitches);
+static GEMItem INPUTFunctionsAssignSwitchesFlightMode("Flight mode:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::variable1)], saveModelChanges);
+static GEMItem INPUTFunctionsAssignSwitchesThrottleHold("Throttle hold:", selectedModel.subtrim[static_cast<uint8_t>(Potentiometer::variable2)], saveModelChanges);
 
 // static char name[5] = "Alex";
 // static GEMSelect myNameSelect(3, (SelectOptionChar[]){{"Alexander", "Alex"}, {"Michael", "Mike"}, {"Peter", "Pete"}});
 // static GEMItem menuItemName("Name:", name, myNameSelect);
 
 void setupMenuPages(){
-  // test page
-  PAGEMain.addMenuItem(INPUTSystemSoundEffects);
-  // PAGEMain.addMenuItem(menuItemName);
+  // Main Menu
+  PAGEMain.addMenuItem(LINKFunctionsSetup);
+  PAGEMain.addMenuItem(LINKSystemSettings);
+
+  // System Settings
+  PAGESystemSettings.addMenuItem(LINKSystemModelNew);
+  PAGESystemSettings.addMenuItem(LINKSystemModelSelect);
+  PAGESystemSettings.addMenuItem(LINKSystemModelUpdate);
+  PAGESystemSettings.addMenuItem(LINKSystemModelCopy);
+  PAGESystemSettings.addMenuItem(LINKSystemModelRemove);
+  PAGESystemSettings.addMenuItem(LINKSystemAuxSwitches);
+  PAGESystemSettings.addMenuItem(INPUTSystemSoundEffects);
+  PAGESystemSettings.addMenuItem(LINKSystemFactoryReset);
+
+  // System Model New
+  PAGESystemModelNew.addMenuItem(INPUTSystemModelNewName);
+  PAGESystemModelNew.addMenuItem(INPUTSystemModelNewKey);
+  PAGESystemModelNew.addMenuItem(BUTTONSystemModelNew);
+
+  // System Model Select
+  // TODO: Add dynamic model selection items
+
+  // System Model Update
+  PAGESystemModelUpdate.addMenuItem(INPUTSystemModelUpdateName);
+  PAGESystemModelUpdate.addMenuItem(INPUTSystemModelUpdateKey);
+  PAGESystemModelUpdate.addMenuItem(BUTTONSystemModelUpdate);
+
+  // System Model Copy
+  PAGESystemModelCopy.addMenuItem(INPUTSystemModelCopyName);
+  PAGESystemModelCopy.addMenuItem(BUTTONSystemModelCopy);
+
+  // System Model Remove
+  PAGESystemModelRemove.addMenuItem(TEXTSystemModelRemove);
+  PAGESystemModelRemove.addMenuItem(BUTTONSystemModelRemove);
+
+  // System Aux Switches
+  PAGESystemAuxSwitches.addMenuItem(INPUTSystemAuxSwitchesA);
+  PAGESystemAuxSwitches.addMenuItem(INPUTSystemAuxSwitchesB);
+  PAGESystemAuxSwitches.addMenuItem(INPUTSystemAuxSwitchesC);
+  PAGESystemAuxSwitches.addMenuItem(INPUTSystemAuxSwitchesD);
+  PAGESystemAuxSwitches.addMenuItem(INPUTSystemAuxSwitchesE);
+  PAGESystemAuxSwitches.addMenuItem(INPUTSystemAuxSwitchesF);
+  PAGESystemAuxSwitches.addMenuItem(INPUTSystemAuxSwitchesG);
+  PAGESystemAuxSwitches.addMenuItem(INPUTSystemAuxSwitchesH);
+
+  // System Factory Reset
+  PAGESystemFactoryReset.addMenuItem(TEXTSystemFactoryReset);
+  PAGESystemFactoryReset.addMenuItem(BUTTONSystemFactoryReset);
+
+  // Functions Setup
+  PAGEFunctionsSetup.addMenuItem(LINKFunctionsReverse);
+  PAGEFunctionsSetup.addMenuItem(LINKFunctionsSubtrim);
+  PAGEFunctionsSetup.addMenuItem(LINKFunctionsAssignSwitches);
+
+  // Functions Reverse
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverse1);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverse2);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverse3);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverse4);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverse5);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverse6);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverseA);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverseB);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverseC);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverseD);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverseE);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverseF);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverseG);
+  PAGEFunctionsReverse.addMenuItem(INPUTFunctionsReverseH);
+
+  // Functions Subtrim
+  PAGEFunctionsSubtrim.addMenuItem(INPUTFunctionsSubtrim1);
+  PAGEFunctionsSubtrim.addMenuItem(INPUTFunctionsSubtrim2);
+  PAGEFunctionsSubtrim.addMenuItem(INPUTFunctionsSubtrim3);
+  PAGEFunctionsSubtrim.addMenuItem(INPUTFunctionsSubtrim4);
+  PAGEFunctionsSubtrim.addMenuItem(INPUTFunctionsSubtrim5);
+  PAGEFunctionsSubtrim.addMenuItem(INPUTFunctionsSubtrim6);
+
+  // Functions Assign Switches
+  PAGEFunctionsAssignSwitches.addMenuItem(INPUTFunctionsAssignSwitchesFlightMode);
+  PAGEFunctionsAssignSwitches.addMenuItem(INPUTFunctionsAssignSwitchesThrottleHold);
+
   menu.setMenuPageCurrent(PAGEMain);
 }
 
